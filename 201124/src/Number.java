20a4f9981bfcc5104f3acc1ef403185a0b0933ce
leticia <leticia@192.168.200.182>
2020-11-28 13:11:42 +0900

    birthdate 년월일 각 자릿수 값, Calendar올해연도, boolean 성인나이판단, double 소수점까지평균구하기

---
+++ 201124/src/Number.java
@@ -0,0 +1,44 @@
+import java.util.Calendar;
+
+public class Number {
+	public static void main(String[] args) {
+		
+	    System.out.println("< 문자열과 수식계산 비교 >");
+		System.out.println("2020-05-10");
+		System.out.println(2020-05-10);
+		
+		System.out.println("< 년 월 일 자릿수의  숫자 각각 가져오기 및 현재연도와 나이 >");
+		int birthdate = 20101010;
+		int year = birthdate/10000;
+		int month = birthdate/100%100;
+		int day = birthdate%100;
+		int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+		int age = thisYear - year + 1;
+		System.out.println("출생 년: "+year);
+		System.out.println("출생 월: " +month);
+		System.out.println("출생 일: "+day);
+		System.out.println("현재연도: "+thisYear);
+		System.out.println("나이: "+age);
+		boolean adult = age >= 20;
+		System.out.println(adult);
+		System.out.println(true);	
+		
+		System.out.println("< 과목 평균을 소수점까지 구하기  - True >");
+		int eng = 80;
+		int kor = 85;
+		double Aver_decimalBracket = (eng+kor)/2.0;
+		double Aver_doubleBracket = (double)(eng+kor)/2;
+		System.out.println(Aver_decimalBracket);		
+		System.out.println(Aver_doubleBracket);
+		
+		System.out.println("< 과목 평균을 소수점까지 구하기  - False >");
+		int intAver = eng+kor/2;
+		double doubleAver = eng+kor/2;
+		double doubleAver_Decimal = eng+kor/2.0;
+		System.out.println(intAver);
+		System.out.println(doubleAver);
+		System.out.println(doubleAver_Decimal);
+		
+		
+        }
+    }
