92e7682e648977e8135cc0b5b18c4dcb99e99efc
leticia <leticia@192.168.200.197>
2021-01-01 15:13:40 +0900

    Calendar.MONTH (0-11), Boolean adult, Double aver

--- 201124/src/Number.java
+++ 201124/src/Number.java
@@ -7,21 +7,32 @@ public class Number {
 		System.out.println("2020-05-10");
 		System.out.println(2020-05-10);
 		
-		System.out.println("< 년 월 일 자릿수의  숫자 각각 가져오기 및 현재연도와 나이 >");
+		System.out.println("< 생년월일에서 연 월 일   숫자 각각 가져오기 / 현재연도 / 나이 >");
 		int birthdate = 20101010;
 		int year = birthdate/10000;
 		int month = birthdate/100%100;
 		int day = birthdate%100;
 		int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+		int thisMonth = Calendar.getInstance().get(Calendar.MONTH); // 0~11
+		int thisDate = Calendar.getInstance().get(Calendar.DATE);
 		int age = thisYear - year + 1;
+		
+		
+		
 		System.out.println("출생 년: "+year);
 		System.out.println("출생 월: " +month);
 		System.out.println("출생 일: "+day);
 		System.out.println("현재연도: "+thisYear);
+		System.out.println("현재몇월: "+thisMonth+1); // 0+11
+		System.out.println("현재몇일: "+thisDate);
 		System.out.println("나이: "+age);
+	
+		
 		boolean adult = age >= 20;
 		System.out.println(adult);
 		System.out.println(true);	
+		
+		
 		
 		System.out.println("< 과목 평균을 소수점까지 구하기  - True >");
 		int eng = 80;
